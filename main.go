a87e53106236845e89c1cd183efbdc8c7ae5ec88
kniarzyn <kniarzyn@gmail.com>
2023-10-08 00:24:57 +0200

    Handle .env files

--- main.go
+++ main.go
@@ -2,8 +2,30 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("Hello world!")
+	err := loadEnvs()
+	if err != nil {
+		log.Fatal("Error while loading .env file: ", err)
+	}
+
+	portString := os.Getenv("PORT")
+
+	fmt.Println("Hello world!:", portString)
+}
+
+func loadEnvs() error {
+	envName := os.Getenv("ENV")
+	if envName == "" {
+		envName = "development"
+	}
+
+	godotenv.Load(".env." + envName)
+	err := godotenv.Load(".env")
+	return err
 }
